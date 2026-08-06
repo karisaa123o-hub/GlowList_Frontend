b5e857ea1a79122c38c0b9929076009480d24d19
karisaa123o-hub <karisaa123o@gmail.com>
2026-08-06 12:53:48 +0700

    update

--- glowlist-frontend/src/pages/AddProduk.jsx
+++ glowlist-frontend/src/pages/AddProduk.jsx
@@ -14,4 +14,27 @@ export default function AddProduk() {
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value});
     };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch("http://localhost:5000/produk", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(formData),
+            });
+            if (res.ok) {
+                alert("Produk berhasil ditambahkan!");
+                navigate("/produk");
+            } else {
+                const data = await res.json();
+                alert(data.message || "Gagal menambahkan produk");
+            }
+        } catch (err) {
+            console.error("Error:", err);
+            alert("Terjadi kesalahan saat menambah produk");
+        }
+    };
+
+    
 }
